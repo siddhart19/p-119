4f15a08357ee49747c099fab5a8ef03f13e6a6a3
siddhart19 <77846186+siddhart19@users.noreply.github.com>
2021-08-06 19:59:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ER2fvlpzy/model.json',modelLoaded);
+}
+function modelLoaded(){
+    console.log("model loaded!");
+}
+function draw(){
+   image(video,0,0,400,400); 
+   classifier.classify(video,gotResult); 
+}
+function gotResult(error,results){
+    if(error){
+console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        }
+}
